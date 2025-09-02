c6b83acebf39f3a24a4dc0c0985b927ec76b00a4
Clyde-kipkoech <kipkoechclyde546@gmail.com>
2025-09-02 09:10:52 +0300

    Fixed views, urls, and templates for list_books and LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,12 +4,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
+
 
 # Function-based view: List all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 
 # Class-based view: Show library details and its books
