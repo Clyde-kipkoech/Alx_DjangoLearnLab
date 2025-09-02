d1bf6275b02185ccc64e702f80327156e470c5dd
Clyde-kipkoech <kipkoechclyde546@gmail.com>
2025-09-02 13:46:47 +0300

    updating to pass the checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
      path('admin-dashboard/', views.admin_view, name='admin_view'),
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
-     path("books/add/", views.add_book, name="add_book"),
-    path("books/edit/<int:pk>/", views.edit_book, name="edit_book"),
-    path("books/delete/<int:pk>/", views.delete_book, name="delete_book"),
+     path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
     path("books/", views.book_list, name="book_list"), 
 ]
