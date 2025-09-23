07728062fcef64b89862c55d411080a7ceebfba2
Clyde-kipkoech <kipkoechclyde546@gmail.com>
2025-09-23 19:25:08 +0300

    week 14 task 2 of the ALX

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+"""
+Views for the Book model using DRF generic views.
+These handle CRUD operations efficiently without writing boilerplate code.
+"""
+
+# List all books / Create a new book
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Retrieve a single book / Update an existing book / Delete a book
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
