502708ba4f385c75765ffcfb1a23e3db63ed5635
Clyde-kipkoech <kipkoechclyde546@gmail.com>
2025-10-01 16:25:08 +0300

    Task 2 one of the ALx week 14

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm, UserUpdateForm, ProfileForm
+from .models import Post   # <-- assuming you have a Post model for the blog
 
-# Registration View
+# ---------------- Existing Views ---------------- #
+
 def register_view(request):
     if request.user.is_authenticated:
         return redirect('home')
@@ -22,7 +26,7 @@ def register_view(request):
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
-# Profile View (Edit & Update User Info)
+
 @login_required
 def profile_view(request):
     if request.method == 'POST':
@@ -41,17 +45,17 @@ def profile_view(request):
 
     return render(request, 'accounts/profile.html', {'u_form': u_form, 'p_form': p_form})
 
-# 🔹 Task Two: Authenticated users can view & edit profile inside blog app
+
 @login_required
 def blog_profile_view(request):
-    if request.method == 'POST':  # <-- checker will see "POST"
+    if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
-            u_form.save()  # <-- checker will see "save()"
+            u_form.save()
             p_form.save()
             messages.success(request, "Blog Profile updated successfully.")
-            return redirect('blog_profile')  # redirect back to this view
+            return redirect('blog_profile')
         else:
             messages.error(request, "Please correct the errors below.")
     else:
@@ -59,3 +63,37 @@ def blog_profile_view(request):
         p_form = ProfileForm(instance=request.user.profile)
 
     return render(request, 'blog/profile.html', {'u_form': u_form, 'p_form': p_form})
+
+
+# ---------------- CRUD Views (Task Requirement) ---------------- #
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']  # adjust fields to match your Post model
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
